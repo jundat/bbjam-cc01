b85e65581685ff90108257daad88b1d8b34a521f
tmthanhtri <tmthanhtri@11e23594-7dc6-70c7-ffe5-e5f166341c1a>
2012-10-19 20:43:26 +0000

    GameResourceConstant.h dung de define tat ca cac resource

---
+++  bbjam-cc01/Classes/GameResourceConstant.h
@@ -0,0 +1,20 @@
+/*
+*/
+#ifndef _GAME_RESOURCE_CONSTANT_H_
+#define _GAME_RESOURCE_CONSTANT_H_
+// Define level
+static char* IMG_LEVEL_PRE = "/levels/level1";
+static char* IMG_LEVEL_POST = ".png";
+
+// Define image
+static char* IMG_CHARACTER = "character.png";
+static char* IMG_ROCKBOXZ = "rockboxz.png";
+static char* IMG_SPIDERBOXZ = "spiderboxz.png";
+static char* IMG_SCRATCH_1 = "scratch_1.png";
+static char* IMG_SCRATCH_2 = "scratch_2.png";
+
+// Define plist
+static char* IMGPLIST_CHARACTER = "character.plist";
+
+
+#endif
