1c8efcbfaef9b0335accd2ab2dc84e78603b9411
tmthanhtri <tmthanhtri@11e23594-7dc6-70c7-ffe5-e5f166341c1a>
2012-10-19 22:35:09 +0000

    Thay doi GameResourceConstant.h them Level

--- trunk/ bbjam-cc01/Classes/Level.h
+++ trunk/ bbjam-cc01/Classes/Level.h
@@ -1,3 +1,4 @@
+
 /*
  * Level.h
  *
@@ -7,10 +8,26 @@
 
 #ifndef LEVEL_H_
 #define LEVEL_H_
-
+#include "cocos2d.h"
+#include "GameResourceConstant.h"
+USING_NS_CC;
 class Level
 {
-
+private:
+	int* m_ArrayID;
+	int* getArrayID(int level)
+	{
+		CCImage* pImage = new CCImage();
+		pImage->initWithImageFile(IMG_LEVEL[level]);
+		CCLOG("Image size W = %i",pImage->getWidth());
+		CCLOG("Image size H = %i",pImage->getHeight());
+		unsigned int* layerPixels = (unsigned int*)pImage->getData();
+	}
+public:
+	Level(int level)
+	{
+		m_ArrayID = getArrayID(level);
+	}
 };
 
 #endif /* LEVEL_H_ */
--- trunk/ bbjam-cc01/Classes/LevelManager.cpp
+++ trunk/ bbjam-cc01/Classes/LevelManager.cpp
@@ -12,15 +12,15 @@ Level* LevelManager::getLevel(int level)
 	return 0;
 }
 
-short** LevelManager::getArray()
-{
-	short **mArray = new short[NUM_GRID_WIDTH][NUM_GRID_HEIGHT];
-	for (int i = 0; i < NUM_GRID_WIDTH; i++)
-	{
-		for (int j = 0; j < NUM_GRID_HEIGHT; j++)
-		{
-			mArray[i][j] = i % 5;
-		}
-	}
-	return mArray;
-}
+//short** LevelManager::getArray()
+//{
+//	short **mArray = new short[NUM_GRID_WIDTH][NUM_GRID_HEIGHT];
+//	for (int i = 0; i < NUM_GRID_WIDTH; i++)
+//	{
+//		for (int j = 0; j < NUM_GRID_HEIGHT; j++)
+//		{
+//			mArray[i][j] = i % 5;
+//		}
+//	}
+//	return mArray;
+//}
