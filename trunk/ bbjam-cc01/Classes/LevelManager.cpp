b8d44545e4ddf1353657f5e48439f581e924d820
nguyentanloc109@gmail.com <nguyentanloc109@gmail.com@11e23594-7dc6-70c7-ffe5-e5f166341c1a>
2012-10-19 19:43:43 +0000

    - add trace width height

--- trunk/ bbjam-cc01/Classes/LevelManager.cpp
+++ trunk/ bbjam-cc01/Classes/LevelManager.cpp
@@ -9,5 +9,18 @@
 
 Level* LevelManager::getLevel(int level)
 {
-	return NULL;
+	return 0;
+}
+
+short** LevelManager::getArray()
+{
+	short **mArray = new short[NUM_GRID_WIDTH][NUM_GRID_HEIGHT];
+	for (int i = 0; i < NUM_GRID_WIDTH; i++)
+	{
+		for (int j = 0; j < NUM_GRID_HEIGHT; j++)
+		{
+			mArray[i][j] = i % 5;
+		}
+	}
+	return mArray;
 }
