7be92ad10038c92a24476513273f9b802196c88d
tmthanhtri <tmthanhtri@11e23594-7dc6-70c7-ffe5-e5f166341c1a>
2012-10-19 22:35:09 +0000

    Thay doi GameResourceConstant.h them Level

---  bbjam-cc01/Classes/GameResourceConstant.h
+++  bbjam-cc01/Classes/GameResourceConstant.h
@@ -3,9 +3,39 @@
 #ifndef _GAME_RESOURCE_CONSTANT_H_
 #define _GAME_RESOURCE_CONSTANT_H_
 // Define level
-static char* IMG_LEVEL_PRE = "/levels/level1";
-static char* IMG_LEVEL_POST = ".png";
-
+//static char* IMG_LEVEL_PRE = "/levels/level1";
+//static char* IMG_LEVEL_POST = ".png";
+//static char* IMG_LEVEL1 = "/levels/level1.png";
+static char* IMG_LEVEL[] = {"",
+	"/levels/level1.png",
+	"/levels/level2.png",
+	"/levels/level3.png",
+	"/levels/level4.png",
+	"/levels/level5.png",
+	"/levels/level6.png",
+	"/levels/level7.png",
+	"/levels/level8.png",
+	"/levels/level9.png",
+	"/levels/level10.png",
+	"/levels/level11.png",
+	"/levels/level12.png",
+	"/levels/level13.png",
+	"/levels/level14.png",
+	"/levels/level15.png",
+	"/levels/level16.png",
+	"/levels/level17.png",
+	"/levels/level18.png",
+	"/levels/level19.png",
+	"/levels/level20.png",
+	"/levels/level21.png",
+	"/levels/level22.png",
+	"/levels/level23.png",
+	"/levels/level24.png",
+	"/levels/level25.png",
+	"/levels/level26.png",
+	"/levels/level27.png",
+	"/levels/level28.png",
+	};
 // Define image
 static char* IMG_CHARACTER = "character.png";
 static char* IMG_ROCKBOXZ = "rockboxz.png";
