18cb3bf1e7b6ed705fa969ef0fcfc2b21e56f41c
longpham.uit@gmail.com <longpham.uit@gmail.com@11e23594-7dc6-70c7-ffe5-e5f166341c1a>
2012-10-20 04:14:23 +0000

    edit conflict in SplashScene.cpp

--- trunk/ bbjam-cc01/Classes/MenuScene.cpp
+++ trunk/ bbjam-cc01/Classes/MenuScene.cpp
@@ -3,7 +3,101 @@
  *
  *  Created on: 20 Oct 2012
  *      Author: Loc
+ *		Edit: long
  */
 
 #include "MenuScene.h"
+#include "SimpleAudioEngine.h"
+#include "MainGameScene.h"
+
+USING_NS_CC;
+
+using namespace CocosDenshion;
+
+
+MenuScene::MenuScene()
+{
+
+}
+
+MenuScene::~MenuScene()
+{
+
+}
+
+CCScene* MenuScene::scene()
+{
+	CCScene *scene = CCScene::create();
+	MenuScene *layer = MenuScene::create();
+	scene->addChild(layer);
+	return scene;
+}
+
+bool MenuScene::init()
+{
+	if (!CCLayer::init())
+	{
+		return false;
+	}
+
+	//init here
+	CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, 0, true);
+	this->setTouchEnabled(true);
+	CCSize s = CCDirector::sharedDirector()->getWinSize();
+	//
+	//background
+	m_spBackground = CCSprite::create("background1.png");
+	m_spBackground->setPosition(ccp(s.width/2, s.height/2));
+	addChild(m_spBackground);
+
+	m_itemPlay = CCMenuItemImage::create("button_up.png", "button_down.png", this, menu_selector(MenuScene::gotoPlay) );
+	m_itemOption = CCMenuItemImage::create("button_up.png", "button_down.png", this, menu_selector(MenuScene::gotoOption) );
+	m_itemTutorial = CCMenuItemImage::create("button_up.png", "button_down.png", this, menu_selector(MenuScene::gotoTurtorial) );
+	m_itemAbout = CCMenuItemImage::create("button_up.png", "button_down.png", this, menu_selector(MenuScene::gotoAbout) );
+	m_itemExit = CCMenuItemImage::create("button_up.png", "button_down.png", this, menu_selector(MenuScene::gotoExit) );
+	//
+	int sw = 768;
+	int sh = 1280;
+	int _space  = 100;
+	
+	m_itemPlay->setPosition(0,0 + _space * 2);
+	m_itemOption->setPosition(0,0 +  _space * 1);
+	m_itemTutorial->setPosition(0,0 +  _space * 0);
+	m_itemAbout->setPosition(0,0 +  _space * -1);
+	m_itemExit->setPosition(0,0 + _space * -2);
+
+	CCMenu* m_Menu =CCMenu::create(m_itemPlay, m_itemOption, m_itemTutorial, m_itemAbout, m_itemExit, NULL);
+
+	addChild(m_Menu);
+
+	//
+	return true;
+}
+
+void MenuScene::gotoPlay(CCObject* sender)
+{
+	CCDirector::sharedDirector()->replaceScene(MainGameScene::scene());
+}
+
+void MenuScene::gotoOption(CCObject* sender)
+{
+
+}
+
+void MenuScene::gotoTurtorial(CCObject* sender)
+{
+
+}
+
+void MenuScene::gotoAbout(CCObject* sender)
+{
+
+}
+
+void MenuScene::gotoExit(CCObject* sender)
+{
+	CCDirector::sharedDirector()->end();
+}
+
+
 
--- trunk/ bbjam-cc01/Classes/MenuScene.h
+++ trunk/ bbjam-cc01/Classes/MenuScene.h
@@ -3,12 +3,41 @@
  *
  *  Created on: 20 Oct 2012
  *      Author: Loc
+		Edit: Long
  */
 
 #ifndef MENUSCENE_H_
 #define MENUSCENE_H_
 
-class MenuScene {
+#include "cocos2d.h"
+USING_NS_CC;
+
+class MenuScene : public CCLayer 
+{
+private:
+	//background
+	CCSprite* m_spBackground;
+
+	//menuitem
+	CCMenuItemImage* m_itemPlay;
+	CCMenuItemImage* m_itemOption;
+	CCMenuItemImage* m_itemTutorial;
+	CCMenuItemImage* m_itemAbout;
+	CCMenuItemImage* m_itemExit;
+
+public:
+	MenuScene();
+	~MenuScene();
+
+	static CCScene* scene();
+	virtual bool init();
+	CREATE_FUNC(MenuScene);
+	
+	void gotoPlay(CCObject* sender);
+	void gotoOption(CCObject* sender);
+	void gotoAbout(CCObject* sender);
+	void gotoTurtorial(CCObject* sender);
+	void gotoExit(CCObject* sender);
 };
 
 #endif /* MENUSCENE_H_ */
